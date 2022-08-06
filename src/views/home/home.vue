a83539157d6743eef40615e55e5224190e3e99ca
BuXSH <1016200331@qq.com>
2022-08-06 17:14:41 +0800

    增加
    增加横屏竖屏，播放方向提示

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,9 +1,9 @@
 <template>
   
-  <div class="home_c">
+  <div class="home_c" ref="home_c">
     <div>
       <!-- 设置框 -->
-      <div class="dingwei" v-show="show">
+      <div class="set" :style="setDiv" v-show="show">
         <button @click="set()">设置</button>
         <div v-show="show_2" @mousedown="this.dragDown" @mouseup="this.dragUp2" >
           <label for="loop">
@@ -13,7 +13,7 @@
             镜像<input id="mirror" class="switch-component" type="checkbox" v-model="mirror">
           </label>
           <label for="direction">
-            滚动方向<input id="direction" class="switch-component" type="checkbox" v-model="direction">
+            滚动方向<input id="direction" class="switch-component" type="checkbox" v-model="direction">{{directionText}}
           </label>
           <br>
           <label for="speed">
@@ -59,13 +59,17 @@
           <label for="color_9">
             <input id="color_9" type="radio" name="backgroundcolor" value="#000000" v-model="divObj.background"> 黑色
           </label>
+          <br>
+          <label for="orientation">
+            {{screenText}}<input id="orientation" class="switch-component" type="checkbox" v-model="screen">
+          </label>
           <div>
             <textarea v-model="text"></textarea>
           </div>
         </div>
       </div>
       <!-- 进度条 -->
-      <div id="progress" :style="setDiv"></div>
+      <div id="progress" :style="progressDiv"></div>
       <!-- 倒计时页面 -->
       <div class="clock" :style="clockDiv">
         <p>
@@ -92,6 +96,13 @@ export default {
   data() {
     return {
       datas:[],
+      setDiv:{
+        'top': 0,
+        transform: 'rotate(0deg)',
+      },
+      //横竖屏
+      screen:false,
+      screenText:'横屏',
       //倒计时div
       clockDiv:{
         display: 'none'
@@ -102,6 +113,7 @@ export default {
       show_2:true,
       //滚动方向
       direction:false,
+      directionText:'向上',
       //倒计时
       countNum:0,
       //播放状态
@@ -120,6 +132,7 @@ export default {
         fontSize: '40px',
 				color: '#ffffff',
         width: 'auto',
+        height: 'auto',
         transform: 'rotateY(0deg)',
         padding: '100vh 0 100vh 0',
         'white-space':'pre-wrap',
@@ -131,7 +144,7 @@ export default {
         background:'#000000',
       },
       //进度条
-      setDiv:{
+      progressDiv:{
         width: '10px',
         height: '0',
       },
@@ -151,28 +164,106 @@ export default {
     })
   },
   watch: {
+    //横竖屏
+    screen:{
+      handler(newValue){
+        //竖屏
+        if(newValue == true){
+          this.screenText = '竖屏'
+          this.pObj['text-orientation'] = 'sideways'
+          this.pObj['writing-mode'] = 'vertical-rl'
+          if(this.direction == true){//向左
+            this.pObj.padding = '100vh 0 100vh 45vw'
+            this.pObj['white-space'] = 'pre'
+            this.progressDiv.width = '10px'
+            setTimeout(()=>{
+              this.$refs.tici.scrollTop = this.$refs.tici.clientHeight/2
+              this.progressDiv.height = (this.$refs.tici.scrollTop/(this.$refs.tici.scrollHeight - this.$refs.tici.clientHeight))*100 + 'vh'
+            },100)
+          }else{//向上
+            this.pObj.height = '97vh'
+            this.pObj.padding = '0 100vw 0 100vw'
+            this.pObj['white-space'] = 'pre-wrap'
+            this.progressDiv.height = '10px'
+            setTimeout(()=>{
+              this.$refs.tici.scrollLeft = this.$refs.tici.clientWidth/2
+              this.progressDiv.width = (this.$refs.tici.scrollLeft/(this.$refs.tici.scrollWidth - this.$refs.tici.clientWidth))*100 + 'vw'
+            },100)
+          } 
+        }else{//横屏
+          this.screenText = '横屏'
+          this.pObj['text-orientation'] = 'mixed'
+          this.pObj['writing-mode'] = 'horizontal-tb'
+          if(this.direction == true){//向左
+            this.pObj.padding = '45vh 100vw 0 100vw'
+            this.pObj['white-space'] = 'pre'
+            this.progressDiv.height = '10px'
+            setTimeout(()=>{
+              this.$refs.tici.scrollLeft = this.$refs.tici.clientWidth/2
+              this.progressDiv.width = (this.$refs.tici.scrollLeft/(this.$refs.tici.scrollWidth - this.$refs.tici.clientWidth))*100 + 'vw'
+            },100)
+          }else{//向上
+            this.pObj.padding = '100vh 0 100vh 0'
+            this.pObj['white-space'] = 'pre-wrap'
+            this.progressDiv.width = '10px'
+            setTimeout(()=>{
+              this.$refs.tici.scrollTop = this.$refs.tici.clientHeight/2
+              this.progressDiv.height = (this.$refs.tici.scrollTop/(this.$refs.tici.scrollHeight - this.$refs.tici.clientHeight))*100 + 'vh'
+            },100)
+          } 
+        }
+        this.mirror = false
+      }
+    },
     //滚动方向
     direction:{
       handler(newValue){
         if(newValue == true){
-          //console.log('向左播放')
-          this.pObj.padding = '45vh 100vw 0 100vw'
-          this.pObj['white-space'] = 'pre'
-          this.setDiv.height = '10px'
-          setTimeout(()=>{
-            this.$refs.tici.scrollLeft = this.$refs.tici.clientWidth/2
-            this.setDiv.width = (this.$refs.tici.scrollLeft/(this.$refs.tici.scrollWidth - this.$refs.tici.clientWidth))*100 + 'vw'
-          },100)
+          this.directionText = '向左'
         }else{
-          //console.log('向上播放')
-          this.pObj.padding = '100vh 0 100vh 0'
-          this.pObj['white-space'] = 'pre-wrap'
-          this.setDiv.width = '10px'
-          setTimeout(()=>{
-            this.$refs.tici.scrollTop = this.$refs.tici.clientHeight/2
-            this.setDiv.height = (this.$refs.tici.scrollTop/(this.$refs.tici.scrollHeight - this.$refs.tici.clientHeight))*100 + 'vh'
-          },100)
+          this.directionText = '向上'
         }
+        //横屏状态
+        if(this.screen == false){
+          //向左播放
+          if(newValue == true){
+            this.pObj.padding = '45vh 100vw 0 100vw'
+            this.pObj['white-space'] = 'pre'
+            this.progressDiv.height = '10px'
+            setTimeout(()=>{
+              this.$refs.tici.scrollLeft = this.$refs.tici.clientWidth/2
+              this.progressDiv.width = (this.$refs.tici.scrollLeft/(this.$refs.tici.scrollWidth - this.$refs.tici.clientWidth))*100 + 'vw'
+            },100)
+          }else{//向上播放
+            this.pObj.padding = '100vh 0 100vh 0'
+            this.pObj['white-space'] = 'pre-wrap'
+            this.progressDiv.width = '10px'
+            setTimeout(()=>{
+              this.$refs.tici.scrollTop = this.$refs.tici.clientHeight/2
+              this.progressDiv.height = (this.$refs.tici.scrollTop/(this.$refs.tici.scrollHeight - this.$refs.tici.clientHeight))*100 + 'vh'
+            },100)
+          }
+        }else{//竖屏状态
+          //向左播放
+          if(newValue == true){
+            this.pObj.padding = '100vh 0 100vh 45vw'
+            this.pObj['white-space'] = 'pre'
+            this.progressDiv.width = '10px'
+            setTimeout(()=>{
+              this.$refs.tici.scrollTop = this.$refs.tici.clientHeight/2
+              this.progressDiv.height = (this.$refs.tici.scrollTop/(this.$refs.tici.scrollHeight - this.$refs.tici.clientHeight))*100 + 'vh'
+            },100)
+          }else{//向上播放
+            this.pObj.padding = '0 100vw 0 100vw'
+            this.pObj['white-space'] = 'pre-wrap'
+            this.progressDiv.height = '10px'
+            setTimeout(()=>{
+              this.$refs.tici.scrollLeft = this.$refs.tici.clientWidth/2
+              this.progressDiv.width = (this.$refs.tici.scrollLeft/(this.$refs.tici.scrollWidth - this.$refs.tici.clientWidth))*100 + 'vw'
+            },100)
+          }
+        }
+        
 
         
       }
@@ -180,12 +271,22 @@ export default {
     //镜像
     mirror:{
       handler(newValue){
-        if(newValue == true){
-          //console.log('镜像')
-          this.pObj.transform = 'rotateY(180deg)'
+        if(this.screen == true){
+          if(newValue == true){
+            //console.log('镜像')
+            this.pObj.transform = 'rotateX(180deg)'
+          }else{
+            //console.log('没镜像')
+            this.pObj.transform = 'rotateX(0deg)'
+          }
         }else{
-          //console.log('没镜像')
-          this.pObj.transform = 'rotateY(0deg)'
+          if(newValue == true){
+            //console.log('镜像')
+            this.pObj.transform = 'rotateY(180deg)'
+          }else{
+            //console.log('没镜像')
+            this.pObj.transform = 'rotateY(0deg)'
+          }
         }
       }
     },
@@ -201,7 +302,7 @@ export default {
   mounted() {
     //初始化文本在提词器中位置
     this.$refs.tici.scrollTop = this.$refs.tici.clientHeight/2
-    this.setDiv.height = (this.$refs.tici.scrollTop/(this.$refs.tici.scrollHeight - this.$refs.tici.clientHeight))*100 + 'vh'
+    this.progressDiv.height = (this.$refs.tici.scrollTop/(this.$refs.tici.scrollHeight - this.$refs.tici.clientHeight))*100 + 'vh'
   },
   methods: {
     _fetchData() {
@@ -237,8 +338,8 @@ export default {
       }else{
         this.$refs.tici.scrollLeft += Math.abs(x)
       }
-      this.setDiv.height = (this.$refs.tici.scrollTop/(this.$refs.tici.scrollHeight - this.$refs.tici.clientHeight))*100 + 'vh'
-      this.setDiv.width = (this.$refs.tici.scrollLeft/(this.$refs.tici.scrollWidth - this.$refs.tici.clientWidth))*100 + 'vw'
+      this.progressDiv.height = (this.$refs.tici.scrollTop/(this.$refs.tici.scrollHeight - this.$refs.tici.clientHeight))*100 + 'vh'
+      this.progressDiv.width = (this.$refs.tici.scrollLeft/(this.$refs.tici.scrollWidth - this.$refs.tici.clientWidth))*100 + 'vw'
     },
     dragUp(){
       this.location.upX = event.clientX
@@ -254,78 +355,146 @@ export default {
       window.removeEventListener("mousemove", this.dragMove)
     },
     touchMove(){
-      this.setDiv.height = (this.$refs.tici.scrollTop/(this.$refs.tici.scrollHeight - this.$refs.tici.clientHeight))*100 + 'vh'
-      this.setDiv.width = (this.$refs.tici.scrollLeft/(this.$refs.tici.scrollWidth - this.$refs.tici.clientWidth))*100 + 'vw'
+      this.progressDiv.height = (this.$refs.tici.scrollTop/(this.$refs.tici.scrollHeight - this.$refs.tici.clientHeight))*100 + 'vh'
+      this.progressDiv.width = (this.$refs.tici.scrollLeft/(this.$refs.tici.scrollWidth - this.$refs.tici.clientWidth))*100 + 'vw'
     },
     //开始播放
     play(){
       //判断现在播放状态
       //true时，开始播放
       if(this.playState == true){
-        //判断播放方向   向上播放
-        if(this.direction == false){
-          //滚动一次
-          this.$refs.tici.scrollTop += this.playSpeed
-          //改变进度条
-          this.setDiv.height = (this.$refs.tici.scrollTop/(this.$refs.tici.scrollHeight - this.$refs.tici.clientHeight))*100 + 'vh'
-          //判断scrollTop位置是否到达底部
-          if(this.$refs.tici.scrollTop < this.$refs.tici.scrollHeight - this.$refs.tici.clientHeight){
-            //11毫秒90帧率
-            setTimeout(this.play,16)
-          }else{  
-            //到底底部时，判断是否需要循环
-            if(this.loop == true){
-              //scrollTop位置重新赋值为0
-              this.$refs.tici.scrollTop = 0
-              this.play()
-            }else{
-              //测试用
-              // console.log('准备结束了')
-              //scrollTop位置重新赋值为0
-              this.$refs.tici.scrollTop = 0
-              //进度条清零
-              this.setDiv.height = '0'
-              //改变播放状态
-              this.playState = !this.playState
-              //改变设置框状态
-              this.show = !this.show
-              //测试用
-              // console.log('结束了')
-              return
+        //判断横屏竖屏   竖屏
+        if(this.screen == true){
+          //判断播放方向   向上播放
+          if(this.direction == false){
+            //滚动一次
+            this.$refs.tici.scrollLeft -= this.playSpeed
+            //改变进度条
+            this.progressDiv.width = (this.$refs.tici.scrollLeft/(this.$refs.tici.scrollWidth - this.$refs.tici.clientWidth))*100 + 'vw'
+            //判断scrollTop位置是否到达底部
+            if(this.$refs.tici.scrollLeft > 0){
+              //11毫秒90帧率
+              setTimeout(this.play,16)
+            }else{  
+              //到底底部时，判断是否需要循环
+              if(this.loop == true){
+                //scrollLeft位置重新赋值到底部
+                this.$refs.tici.scrollLeft = this.$refs.tici.scrollWidth - this.$refs.tici.clientWidth
+                this.play()
+              }else{
+                //测试用
+                // console.log('准备结束了')
+                //scrollLeft位置重新赋值到底部
+                this.$refs.tici.scrollLeft = this.$refs.tici.scrollWidth - this.$refs.tici.clientWidth
+                //进度条清零
+                this.progressDiv.width = '0'
+                //改变播放状态
+                this.playState = !this.playState
+                //改变设置框状态
+                this.show = !this.show
+                //测试用
+                // console.log('结束了')
+                return
+              }
+            }
+          }else{   //向左播放
+            //滚动一次
+            this.$refs.tici.scrollTop += this.playSpeed
+            //改变进度条
+            this.progressDiv.height = (this.$refs.tici.scrollTop/(this.$refs.tici.scrollHeight - this.$refs.tici.clientHeight))*100 + 'vh'
+            //判断scrollTop位置是否到达底部
+            if(this.$refs.tici.scrollTop < this.$refs.tici.scrollHeight - this.$refs.tici.clientHeight){
+              //11毫秒90帧率
+              setTimeout(this.play,16)
+            }else{  
+              //到底底部时，判断是否需要循环
+              if(this.loop == true){
+                //scrollTop位置重新赋值为0
+                this.$refs.tici.scrollTop = 0
+                this.play()
+              }else{
+                //测试用
+                // console.log('准备结束了')
+                //scrollTop位置重新赋值为0
+                this.$refs.tici.scrollTop = 0
+                //进度条清零
+                this.progressDiv.height = '0'
+                //改变播放状态
+                this.playState = !this.playState
+                //改变设置框状态
+                this.show = !this.show
+                //测试用
+                // console.log('结束了')
+                return
+              }
             }
           }
-        }else{   //向左播放
-          //滚动一次
-          this.$refs.tici.scrollLeft += this.playSpeed
-          //改变进度条
-          this.setDiv.width = (this.$refs.tici.scrollLeft/(this.$refs.tici.scrollWidth - this.$refs.tici.clientWidth))*100 + 'vw'
-          //判断scrollLeft位置是否到达底部
-          if(this.$refs.tici.scrollLeft < this.$refs.tici.scrollWidth - this.$refs.tici.clientWidth){
-            //11毫秒90帧率
-            setTimeout(this.play,16)
-          }else{  
-            //到底底部时，判断是否需要循环
-            if(this.loop == true){
-              //scrollLeft位置重新赋值为0
-              this.$refs.tici.scrollLeft = 0
-              this.play()
-            }else{
-              //测试用
-              // console.log('准备结束了')
-              //scrollLeft位置重新赋值为0
-              this.$refs.tici.scrollLeft = 0
-              //进度条清零
-              this.setDiv.width = '0'
-              //改变播放状态
-              this.playState = !this.playState
-              //改变设置框状态
-              this.show = !this.show
-              //测试用
-              // console.log('结束了')
-              return
+        }else{  //横屏
+          //判断播放方向   向上播放
+          if(this.direction == false){
+            //滚动一次
+            this.$refs.tici.scrollTop += this.playSpeed
+            //改变进度条
+            this.progressDiv.height = (this.$refs.tici.scrollTop/(this.$refs.tici.scrollHeight - this.$refs.tici.clientHeight))*100 + 'vh'
+            //判断scrollTop位置是否到达底部
+            if(this.$refs.tici.scrollTop < this.$refs.tici.scrollHeight - this.$refs.tici.clientHeight){
+              //11毫秒90帧率
+              setTimeout(this.play,16)
+            }else{  
+              //到底底部时，判断是否需要循环
+              if(this.loop == true){
+                //scrollTop位置重新赋值为0
+                this.$refs.tici.scrollTop = 0
+                this.play()
+              }else{
+                //测试用
+                // console.log('准备结束了')
+                //scrollTop位置重新赋值为0
+                this.$refs.tici.scrollTop = 0
+                //进度条清零
+                this.progressDiv.height = '0'
+                //改变播放状态
+                this.playState = !this.playState
+                //改变设置框状态
+                this.show = !this.show
+                //测试用
+                // console.log('结束了')
+                return
+              }
+            }
+          }else{   //向左播放
+            //滚动一次
+            this.$refs.tici.scrollLeft += this.playSpeed
+            //改变进度条
+            this.progressDiv.width = (this.$refs.tici.scrollLeft/(this.$refs.tici.scrollWidth - this.$refs.tici.clientWidth))*100 + 'vw'
+            //判断scrollLeft位置是否到达底部
+            if(this.$refs.tici.scrollLeft < this.$refs.tici.scrollWidth - this.$refs.tici.clientWidth){
+              //11毫秒90帧率
+              setTimeout(this.play,16)
+            }else{  
+              //到底底部时，判断是否需要循环
+              if(this.loop == true){
+                //scrollLeft位置重新赋值为0
+                this.$refs.tici.scrollLeft = 0
+                this.play()
+              }else{
+                //测试用
+                // console.log('准备结束了')
+                //scrollLeft位置重新赋值为0
+                this.$refs.tici.scrollLeft = 0
+                //进度条清零
+                this.progressDiv.width = '0'
+                //改变播放状态
+                this.playState = !this.playState
+                //改变设置框状态
+                this.show = !this.show
+                //测试用
+                // console.log('结束了')
+                return
+              }
             }
           }
-        } 
+        }
       }else{
         //测试用
         // console.log('暂停了')
